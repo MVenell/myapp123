ad4868b8e529ffbad5198e47544e6e3a06b70c90
ali.venell <ali.venell@gmail.com>
2025-06-25 10:36:04 +0000

    new changes again

--- lib/ai_page.dart
+++ lib/ai_page.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 
 class AiPage extends StatelessWidget {
-  const AiPage({Key? key}) : super(key: key);
+  const AiPage({super.key});
 
   @override
   Widget build(BuildContext context) {
